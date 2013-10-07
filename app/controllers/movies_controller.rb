4de57db1a04f1c32cea3404e43142ca68cdcbbbb
David Seeto <seeto.david@gmail.com>
2013-10-06 23:11:54 -0700

    Working parts 1, 2, and 3.

--- app/controllers/movies_controller.rb
+++ app/controllers/movies_controller.rb
@@ -1,13 +1,39 @@
 class MoviesController < ApplicationController
-
+  
   def show
     id = params[:id] # retrieve movie ID from URI route
     @movie = Movie.find(id) # look up movie by unique ID
     # will render app/views/movies/show.<extension> by default
   end
 
-  def index
-    @movies = Movie.all
+  def index 
+    session[:sort] = params[:sort] if params[:sort]
+    session[:ratings] = params[:ratings] if params[:ratings]
+    
+    if (!params[:ratings] || !params[:sort]) && session[:ratings] && session[:sort]
+      flash.keep
+      redirect_to movies_path :ratings => session[:ratings], :sort => session[:sort] and return
+    elsif !params[:ratings] && session[:ratings]
+      flash.keep
+      redirect_to movies_path :ratings => session[:ratings] and return
+    elsif !params[:sort] && session[:sort]
+      flash.keep
+      redirect_to movies_path :sort => session[:sort] and return
+    end
+
+    @all_ratings = Movie.ratings
+
+    @checked_ratings = params[:ratings] ? params[:ratings].keys : @all_ratings
+    @all_ratings = Movie.ratings
+    if params[:sort] == "title"
+      @movies = Movie.where(:rating => @checked_ratings).sort_by(&:title)
+      @title_sorted = 'hilite'
+    elsif params[:sort] == "date"
+      @movies = Movie.where(:rating => @checked_ratings).sort_by(&:release_date)
+      @date_sorted = 'hilite'
+    else
+      @movies = Movie.where(:rating => @checked_ratings)
+    end
   end
 
   def new
--- app/models/movie.rb
+++ app/models/movie.rb
@@ -1,2 +1,7 @@
 class Movie < ActiveRecord::Base
+
+  def Movie.ratings
+    ['G', 'PG', 'PG-13', 'R']
+  end
+
 end
